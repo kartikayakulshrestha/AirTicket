505b65139c66f3f1a7e2e73005d49be2ce3d2e81
kartikayakulshrestha <kartikaya.kul0908@gmail.com>
2024-07-17 19:31:53 +0530

    title changed , toggle corrected , try another route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios"
+import axios from "axios";
 import "./App.css";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { IoMdArrowDropup } from "react-icons/io";
@@ -9,17 +9,17 @@ function App() {
   const [cabinOptions, setcabinOptions] = useState("hidden");
   const [originValue, setOriginValue] = useState("SYD");
   const [destinationValue, setDestinationValue] = useState("SYD");
-  const [toggle,settoggle]=useState(0)
+  const [toggle, settoggle] = useState(0);
   const [s, ss] = useState("SYD");
   const [destinationOptions, setDestinationOptions] = useState("hidden");
-  
+  const [flag, setflag] = useState(0);
   const [d, dd] = useState("JFK");
   const [l, ll] = useState("Economy");
-  const [ticketData,setTicketData]=useState([])
+  const [ticketData, setTicketData] = useState([]);
   const orginAirport = ["JFK", "DEL", "SYD", "BOM", "BNE", "BLR"];
   const destinationAirport = ["JFK", "DEL", "SYD", "LHR", "CDG", "DOH", "SIN"];
   const cabin = ["economy", "business", "first"];
-  
+
   function handleorginchange(e) {
     let l = e.target.value;
     ss(l);
@@ -39,60 +39,69 @@ function App() {
     port.includes(d.toUpperCase())
   );
   const filteredcabin = cabin.filter((cab) => cab.includes(l.toLowerCase()));
-  async function handleSubmit(){
-    let ori=orginAirport.filter((x)=>{return x==s})
-    let dest=destinationAirport.filter((x)=>{return x==d})
-    let cab=cabin.filter((x)=>{return x==l.toLowerCase()})
-    
-    if (ori.length==0 || dest.length==0 || cab.length==0){
+  async function handleSubmit() {
+    let ori = orginAirport.filter((x) => {
+      return x == s;
+    });
+    let dest = destinationAirport.filter((x) => {
+      return x == d;
+    });
+    let cab = cabin.filter((x) => {
+      return x == l.toLowerCase();
+    });
+
+    if (ori.length == 0 || dest.length == 0 || cab.length == 0) {
       alert("fill correctly");
-    } 
+      return;
+    }
+
     try {
-      setDestinationValue(dest[0])
-      setOriginValue(ori[0])
+      setDestinationValue(dest[0]);
+      setOriginValue(ori[0]);
       const response = await axios.post(
-        'https://cardgpt.in/apitest',
+        "https://cardgpt.in/apitest",
         {
           origin: ori[0],
           destination: dest[0],
           partnerPrograms: [
-            'Air Canada',
-            'United Airlines',
-            'KLM',
-            'Qantas',
-            'American Airlines',
-            'Etihad Airways',
-            'Alaska Airlines',
-            'Qatar Airways',
-            'LifeMiles'
+            "Air Canada",
+            "United Airlines",
+            "KLM",
+            "Qantas",
+            "American Airlines",
+            "Etihad Airways",
+            "Alaska Airlines",
+            "Qatar Airways",
+            "LifeMiles",
           ],
           stops: 2,
-          departureTimeFrom: '2024-07-16T07:41:57.606Z',
-          departureTimeTo: '2024-08-16T07:42:56.816Z',
+          departureTimeFrom: "2024-07-16T07:41:57.606Z",
+          departureTimeTo: "2024-08-16T07:42:56.816Z",
           isOldData: false,
           limit: 302,
           offset: 0,
-          cabinSelection: [cab[0].toUpperCase()+cab.slice(1,cab.length)],
-          date: '2024-07-09T12:00:17.796Z'
+          cabinSelection: [cab[0].toUpperCase() + cab.slice(1, cab.length)],
+          date: "2024-07-09T12:00:17.796Z",
         },
         {
           headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Accept-Language': 'en-US,en;q=0.9,hi;q=0.8',
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
-          }
+            Accept: "application/json, text/plain, */*",
+            "Accept-Language": "en-US,en;q=0.9,hi;q=0.8",
+            "Cache-Control": "no-cache",
+            "Content-Type": "application/json",
+            "User-Agent":
+              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+          },
         }
       );
-      let res=response.data.data
-      setTicketData(res)
-      
-
+      let res = response.data.data;
+      if (res.length == 0) {
+        setflag(1);
+      }
+      setTicketData(res);
     } catch (error) {
-      console.error('Error fetching data', error);
+      console.error("Error fetching data", error);
     }
-  
   }
 
   return (
@@ -367,86 +376,164 @@ function App() {
             </div>
             {/* sections */}
             <div className="bg-[#181818] ml-2 mb-8 h-16 w-64 hover:bg-[#1c2519]">
-            <div className={`flex py-5 ${cabinOptions == "hidden" ? "" : "hidden"} ${destinationOptions == "hidden" ? "" : "hidden"}`}>
-              <div className="mx-5">  
-                  <button className={` ${toggle?"bg-slate-600":"bg-orange-700"} my-1 h-4 w-8 rounded-lg relative`}  onClick={()=>{toggle?settoggle(0):settoggle(1)}}>
-                    <div className={` h-4 w-4 absolute top-0 rounded-full ${toggle?"right-0 bg-slate-800":"left-0 bg-slate-300"}`}></div>
+              <div
+                className={`flex py-5 ${
+                  cabinOptions == "hidden" ? "" : "hidden"
+                } ${destinationOptions == "hidden" ? "" : "hidden"}`}
+              >
+                <div className="mx-5">
+                  <button
+                    className={` ${
+                      toggle == 0 ? "bg-slate-600" : "bg-orange-700"
+                    } my-1 h-4 w-8 rounded-lg relative`}
+                    onClick={() => {
+                      toggle == 0 ? settoggle(1) : settoggle(0);
+                    }}
+                  >
+                    <div
+                      className={` h-4 w-4 absolute top-0 rounded-full ${
+                        toggle == 0
+                          ? "right-0 bg-slate-800"
+                          : "left-0 bg-slate-300"
+                      }`}
+                    ></div>
                   </button>
+                </div>
+                <p
+                  className={`${
+                    toggle == 0 ? "text-[#5e5e5e]" : "text-[#e8e4e4]"
+                  }`}
+                >
+                  Show{" "}
+                  <span
+                    className={`${
+                      toggle == 0 ? "text-[#834218]" : "text-orange-500"
+                    }`}
+                  >
+                    Pro Filters
+                  </span>
+                </p>
               </div>
-              <p className={`${toggle?"text-[#5e5e5e]":"text-[#e8e4e4]"}`}>Show <span className={`${toggle?"text-[#834218]":"text-orange-500"}`}>Pro Filters</span></p>
             </div>
-            </div>
-            
-            <button className="bg-[#38b8a6] mb-10 text-white py-2 px-6 rounded-md" onClick={handleSubmit}>Search</button>
-            
+
+            <button
+              className="bg-[#38b8a6] mb-10 text-white py-2 px-6 rounded-md"
+              onClick={handleSubmit}
+            >
+              Search
+            </button>
+
             <div className="grid grid-cols-11">
-            <div className="col-span-1 "></div>
-            <div className="col-span-10">
-              <div className="grid md:grid-cols-2 gap-4">
-                
-                  {ticketData.length?
-                  ticketData.map((dataa,index)=>{
-                    return <div key={index}>
-                      <div className="bg-[#346c4a] col-span-1 h-[350px] rounded-lg">
-                    <div className="grid text-white justify-center">
-                      <img src="" alt="Logo" className="px-16 pt-6"></img>
-                      <h3 className="text-2xl my-1 font-normal text-center">{dataa.partner_program }</h3>
-                      <p className="text-center my-1">{originValue +" -> "+ destinationValue } </p>
-                      <p className="text-center my-1">2024-07-09 - 2024-10-07</p> 
-                      {dataa.min_business_miles==null?
-                      <h1 className="text-2xl text-center">N/A</h1>:
-                      <div className="flex"><h1 className="text-2xl ml-10">{dataa.min_business_miles}</h1>
-                      {dataa.min_business_tax==null?"":
-                      <p className="pt-1"> +${dataa.min_business_tax}</p>
-                      }
-                      </div>
-                      
-                      
-                      }
-                      <p className="text-center my-1" >Min Business Miles</p>
-                      {dataa.min_economy_miles==null?
-                      <h1 className="text-2xl text-center">N/A</h1>:
-                      <div className="flex"><h1 className={`text-2xl ml-10 ${dataa.min_economy_taxs==null?"mx-12":""} `}>{dataa.min_economy_miles}</h1>
-                      {dataa.min_economy_taxs==null?"":
-                      <p className="pt-1"> +${dataa.min_economy_taxs}</p>
-                      }
-                      </div>
-                      
-                      
-                      }
-                      
-                      
-                      <p className="text-center my-1" >Min Economy Miles</p>
-                      {dataa.min_first_miles==null?
-                      <h1 className="text-2xl text-center">N/A</h1>:
-                      <div className="flex"><h1 className="text-2xl ml-10">{dataa.min_first_miles}</h1>
-                      {dataa.min_first_taxs==null?"":
-                      <p className="pt-1"> +${dataa.min_first_taxs}</p>
-                      }
-                      </div>
-                      
-                      
-                      }
-                      
-                      <p className="text-center">Min first Miles</p>
-                    </div>
-                  </div>
-                    </div>
-                  })
-                  :""}
-                
-                <div className=" col-span-1"></div>
+              <div className="col-span-1 "></div>
+              <div className="col-span-10">
+                {flag ? (
+                  <h2 className="text-white text-center text-xl">
+                    Try another search route
+                  </h2>
+                ) : (
+                  ""
+                )}
+                <div className="grid md:grid-cols-2 gap-4">
+                  {ticketData.length
+                    ? ticketData.map((dataa, index) => {
+                        return (
+                          <div key={index}>
+                            <div className="bg-[#346c4a] col-span-1 h-[350px] rounded-lg">
+                              <div className="grid text-white justify-center">
+                                <img
+                                  src=""
+                                  alt="Logo"
+                                  className="px-16 pt-6"
+                                ></img>
+                                <h3 className="text-2xl my-1 font-normal text-center">
+                                  {dataa.partner_program}
+                                </h3>
+                                <p className="text-center my-1">
+                                  {originValue + " -> " + destinationValue}{" "}
+                                </p>
+                                <p className="text-center my-1">
+                                  2024-07-09 - 2024-10-07
+                                </p>
+                                {dataa.min_business_miles == null ? (
+                                  <h1 className="text-2xl text-center">N/A</h1>
+                                ) : (
+                                  <div className="flex">
+                                    <h1 className="text-2xl ml-10">
+                                      {dataa.min_business_miles}
+                                    </h1>
+                                    {dataa.min_business_tax == null ? (
+                                      ""
+                                    ) : (
+                                      <p className="pt-1">
+                                        {" "}
+                                        +${dataa.min_business_tax}
+                                      </p>
+                                    )}
+                                  </div>
+                                )}
+                                <p className="text-center my-1">
+                                  Min Business Miles
+                                </p>
+                                {dataa.min_economy_miles == null ? (
+                                  <h1 className="text-2xl text-center">N/A</h1>
+                                ) : (
+                                  <div className="flex">
+                                    <h1
+                                      className={`text-2xl ml-10 ${
+                                        dataa.min_economy_taxs == null
+                                          ? "mx-12"
+                                          : ""
+                                      } `}
+                                    >
+                                      {dataa.min_economy_miles}
+                                    </h1>
+                                    {dataa.min_economy_taxs == null ? (
+                                      ""
+                                    ) : (
+                                      <p className="pt-1">
+                                        {" "}
+                                        +${dataa.min_economy_taxs}
+                                      </p>
+                                    )}
+                                  </div>
+                                )}
+
+                                <p className="text-center my-1">
+                                  Min Economy Miles
+                                </p>
+                                {dataa.min_first_miles == null ? (
+                                  <h1 className="text-2xl text-center">N/A</h1>
+                                ) : (
+                                  <div className="flex">
+                                    <h1 className="text-2xl ml-10">
+                                      {dataa.min_first_miles}
+                                    </h1>
+                                    {dataa.min_first_taxs == null ? (
+                                      ""
+                                    ) : (
+                                      <p className="pt-1">
+                                        {" "}
+                                        +${dataa.min_first_taxs}
+                                      </p>
+                                    )}
+                                  </div>
+                                )}
+
+                                <p className="text-center">Min first Miles</p>
+                              </div>
+                            </div>
+                          </div>
+                        );
+                      })
+                    : ""}
+
+                  <div className=" col-span-1"></div>
+                </div>
               </div>
             </div>
-          
-            
-            </div>
-
           </div>
 
-          <div className="col-span-1 sm:col-span-3  xl:col-span-4 ">
-           
-          </div>
+          <div className="col-span-1 sm:col-span-3  xl:col-span-4 "></div>
         </div>
       </div>
     </>
